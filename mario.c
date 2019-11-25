723ebc5026d8c6e26348ad0d04a17ad3d03d9c92
Evheniy-Kartavtsev <39993242+Evheniy-Kartavtsev@users.noreply.github.com>
2019-11-25 17:33:38 +0200

    Add files via upload

---
+++ mario.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void) {
+
+	int num, str, sym, prob;
+	char sl = '#';
+
+	do {																			// цикл перевіряє чи введено числа від 1 до 8																		
+		num = get_int("Enter height: ");	} 		
+	while (num < 1 || num > 8);
+
+	for (str = 0; str != num; str++) {												// цикл к-сті рядків
+		
+		for (prob = num - str; prob > 1; prob--)	{								// цикл к-сті пробілів
+            	printf(" ");	}
+		
+		for (sym = -1; sym != str; sym++) 	{										// цикл	к-сть символів зліва
+				printf("%c", sl);	}	
+		
+		printf("\n");	}
+return 0;
+}
